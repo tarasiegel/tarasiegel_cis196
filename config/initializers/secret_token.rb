ad2914439d5d87638fdfcca7641dd08cd60c376c
Jennifer Cahalane <cahalane.jennifer@gmail.com>
2013-09-17 02:02:57 -0400

    homework 2, forms etc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaraSiegelHomework2::Application.config.secret_token = '180550743e31c176dcab4d9698ce87e10197d96707fb8f42d22cc24b284feba528dc97cd5f2c31b6ba9c2b464e26775e912a05d98de51d90cf388a699fd71a88'
