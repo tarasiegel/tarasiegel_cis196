4fefcb6312820cc863dacae066669ee339fd2736
Tara Siegel <Tara@Tara-Siegels-MacBook-Pro.local>
2013-10-10 21:04:24 -0400

    styling and shit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :body, :date, :title, :user_id
+
+  validates :title, presence: true, length: {minimum: 2}
+  validates :body, presence: true, length: {minimum: 5}
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password
+  validates :name, presence: true
+
+  emailFormatValidation = /[a-z.-]+@[a-z \d -.]+[.][a-z]+/i
+  validates :email, format: {with: emailFormatValidation}, uniqueness: true
+
+  validates :password, length: {within: 3..20, too_long: "is way too many characters. Must be less than 20.", too_short: "needs to be more than 3 characters por favor"}, confirmation: true
 end
