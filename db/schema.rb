29dd7d4586c27e49aa7722b4b474d9896d695a62
Tara Siegel <Tara@Tara-Siegels-MacBook-Pro.local>
2013-10-27 17:15:10 -0400

    email migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131027175719) do
+ActiveRecord::Schema.define(:version => 20131027211341) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
