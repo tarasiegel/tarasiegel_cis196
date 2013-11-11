c51a06bb67661d12015148771c05aa875094c1fd
Tara Siegel <Tara@Tara-Siegels-MacBook-Pro.local>
2013-11-10 19:38:31 -0500

    wont go to a new post if not signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,11 @@ class PostsController < ApplicationController
   end
 
   def new
-  	@post = Post.new
+  	if current_user
+      @post = Post.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
